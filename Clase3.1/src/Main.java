9c5cd65c03438eb9ff0cd3dc9febff8476055908
JoelTorres981 <joel.torres@epn.edu.ec>
2024-10-17 09:23:14 -0500

    EjercicioenClase

---
+++ Clase3.1/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Jugador jugador1 = new Jugador();
+
+        jugador1.patear();
+        jugador1.correr();
+
+        Jugador jugador2 = new Jugador("Pepe","Argentina",18);
+
+        jugador2.patear();
+        jugador2.correr();
+        jugador2.saltar();
+
+    }
+}
